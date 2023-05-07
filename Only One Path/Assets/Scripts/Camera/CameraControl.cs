8b32ff459d3e76ec39e7364981ebcaef4d802484
salmafawzy <77148838+SalmaElarabyFawzy@users.noreply.github.com>
2023-05-07 13:08:06 +0300

    new commit

---
+++ Only One Path/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CameraControl : MonoBehaviour
+{
+    [Header("Player")]
+    private Transform player;
+
+
+    [Header("Camera")]
+    [SerializeField] float mouseSenstivity = 3f;
+    private float mouseXdir = 0;
+    private Vector2 MouseDir;
+    private CameraControle _camera;
+  
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        _camera = new CameraControle();
+    }
+
+
+    private void OnEnable()
+    {
+        _camera.Enable();
+        _camera.FPSCamera.MouseX.performed += cxt => MouseDir.x = cxt.ReadValue<float>();
+        _camera.FPSCamera.MouseY.performed += _   => MouseDir.y = _.ReadValue<float>();
+        _camera.FPSCamera.MouseX.canceled  += cxt => MouseDir.x = 0f;
+        _camera.FPSCamera.MouseY.canceled  += _   => MouseDir.y = 0f;
+    }
+
+
+    private void OnDisable()
+    {
+        _camera.Disable();
+    }
+   
+
+
+    void Update()
+    {
+       Rotation();
+    }
+
+
+
+    void Rotation()
+    {
+        float mouseX = MouseDir.x * mouseSenstivity * Time.deltaTime;
+        float mouseY = MouseDir.y * mouseSenstivity * Time.deltaTime;
+         mouseXdir -= mouseY;
+         mouseXdir = Mathf.Clamp(mouseXdir, -50f, 30f);   
+         transform.localRotation = Quaternion.Euler(mouseXdir,0, 0);
+         player.Rotate(Vector3.up*mouseX);
+    }
+}
---
+++ Only One Path/Assets/Scripts/Player/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Player")]
+
+    [SerializeField] private float speed = 10f;
+    private CharacterController _controller;
+    private PlayerContrpoller _playerContrpoller;
+    private Vector2 Input;
+    private AudioSource _sound;
+
+
+    [Header("Camera")]
+
+    private Transform camera;
+
+   
+
+    private void Awake()
+    {
+        _playerContrpoller = new PlayerContrpoller();
+        _controller = GetComponent<CharacterController>();
+         camera = transform.GetChild(0).transform;
+        _sound = GetComponent<AudioSource>();
+       
+    }
+
+
+
+    private void OnEnable()
+    {
+        _playerContrpoller.Enable();
+        _playerContrpoller.Player.Movement.performed += cxt => Input = cxt.ReadValue<Vector2>();
+        _playerContrpoller.Player.Movement.canceled += cxt => Input = Vector2.zero;
+
+    }
+
+
+
+    private void OnDisable()
+    {
+        _playerContrpoller.Disable();
+    }
+ 
+
+
+    private void Update()
+    {
+        Movement(Input);
+
+    }
+
+
+    private void Movement(Vector2 cxt)
+    {
+        float WS = cxt.y;
+        float AD = cxt.x;
+        Vector3 move = (transform.forward* WS+transform.right*AD).normalized;
+        if(move.magnitude > 0.1)
+        {
+           _sound.enabled = true;
+          _controller.Move(move * speed * Time.deltaTime);
+       
+        }
+        else  _sound.enabled = false;
+      
+    }
+
+}
